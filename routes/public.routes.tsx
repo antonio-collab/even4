826fc43a6a565ad188ea705e4028090f760cce9f
mateuswdo <mateusweslley@acad.ifma.edu.br>
2025-01-22 16:21:17 -0300

    adicionando tipagem de rotas em rotas de autenticação

--- routes/public.routes.tsx
+++ routes/public.routes.tsx
@@ -1,9 +1,17 @@
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createNativeStackNavigator, NativeStackNavigationProp } from "@react-navigation/native-stack";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 
+type AuthRoutes = {
+    login: undefined
+    register: undefined
+}
+
+export type AuthNavigatorRoutesProps = NativeStackNavigationProp<AuthRoutes>;
+
+const {Navigator, Screen} = createNativeStackNavigator<AuthRoutes>()
+
 export default function PublicRoutes() {
-const {Navigator, Screen} = createNativeStackNavigator()
 
     return (
         <Navigator screenOptions={{headerShown: false}}>
