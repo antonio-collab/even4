cc29cce276879cad155f24482bdb5f1768b1fc48
mateuswdo <149194918+mateuswdo@users.noreply.github.com>
2025-01-25 15:25:24 -0300

    retirando headerTitle das telas Register, Login e Home

--- routes/public.routes.tsx
+++ routes/public.routes.tsx
@@ -1,23 +1,35 @@
-import { createNativeStackNavigator, NativeStackNavigationProp } from "@react-navigation/native-stack";
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from "@react-navigation/native-stack";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import Home from "../pages/Home";
 
 type AuthRoutes = {
-    login: undefined
-    register: undefined
-}
+  login: undefined;
+  register: undefined;
+  home: undefined;
+};
 
 export type AuthNavigatorRoutesProps = NativeStackNavigationProp<AuthRoutes>;
 
-const {Navigator, Screen} = createNativeStackNavigator<AuthRoutes>()
+const { Navigator, Screen } = createNativeStackNavigator<AuthRoutes>();
 
 export default function PublicRoutes() {
-
-    return (
-        <Navigator screenOptions={{headerShown: false}}>
-            <Screen name="login" component={Login} />
-            <Screen name="register" component={Register} />
-      </Navigator>
-
-    )
+  return (
+    <Navigator screenOptions={{ headerShown: true }}>
+      <Screen name="home" component={Home} options={{headerShown: false}}/>
+      <Screen name="login" component={Login} options={{
+          headerTitle: "", // Remove o título "register" do cabeçalho
+        }}/>
+      <Screen
+        name="register"
+        component={Register}
+        options={{
+          headerTitle: "", // Remove o título "register" do cabeçalho
+        }}
+      />
+    </Navigator>
+  );
 }
